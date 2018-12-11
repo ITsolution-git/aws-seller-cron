6b49aa7f21b36ac330e2abd21cc4cbf84d6d732e
coseasonruby <coseasonruby@gmail.com>
2018-12-11 15:14:24 -0500

    update api asyc

--- index.js
+++ index.js
@@ -31,10 +31,9 @@ var myClient = new OneSignal.Client({
    app: { appAuthKey: 'OTZmZTcwZDYtMWI1MC00NmE1LThlMjItODllZjg3MjUyMTk3', appId: '0b6427d6-620b-457e-bd39-2cb8058ff542' }
 });  
 
-// order.OrderTotal.Amount
-// order.OrderTotal.CurrencyCode
 
 cron.schedule('5 * * * *', () => {
+	console.log('!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!')
 	MongoClient.connect(URL, function(err, db) {
 	    if (err) throw err;
 	    var collection = db.collection("users");
@@ -42,41 +41,49 @@ cron.schedule('5 * * * *', () => {
 			.find({})
 			.toArray()
 			.then(result => {
-		        result.map( (res) => {
+		        result.map(async (res) => {
 					var amazonMws = require('amazon-mws')(process.env.AWS_ACCESS_KEY, process.env.SECRET_KEY);	
 					// var diff = Math.abs(new Date() - new Date(res['last_date']));
 					// var minutes = Math.floor((diff/1000)/60);
-		         	amazonMws.orders.search({
-					    'Version': '2013-09-01',
-					    'Action': 'ListOrders',
-					    'SellerId': res['seller_id'],
-					    'MWSAuthToken': res['mws_auth_token'],
-		             	'MarketplaceId.Id.1': res['market_place_id'],
-					    'LastUpdatedAfter': res['last_date']
-					    // 'LastUpdatedAfter': new Date(2018,11,6,)
-					}, function (error, response) {
-					    if (error) {
-					        console.log('error ', error);
-					        return;
-					    }
-					    // response.Orders.Order.map(console.log);
-				       	response.Orders.Order.map( (order) => {
-				       		if (order['OrderStatus'] != 'Shipped'){
-					       		amazonMws.orders.search({
+					let reponse;
+					try {
+			         	response = await amazonMws.orders.search({
+				        	'Version': '2013-09-01',
+						    'Action': 'ListOrders',
+						    'SellerId': res['seller_id'],
+						    'MWSAuthToken': res['mws_auth_token'],
+			             	'MarketplaceId.Id.1': res['market_place_id'],
+						    // 'LastUpdatedAfter': res['last_date']
+						    'LastUpdatedAfter': new Date(2018,11,6,),
+						    'OrderStatus.Status.1': 'Pending'
+						});
+					    for (var i = 0; i < response.Orders.Order.length; i++) {
+					    	let order = response.Orders.Order[i];
+					    	try{
+					       		let responseOrderItem = await amazonMws.orders.search({
 								    'Version': '2013-09-01',
 								    'Action': 'ListOrderItems',
 								    'SellerId': res['seller_id'],
 								    'MWSAuthToken': res['mws_auth_token'],
-								    'AmazonOrderId' : order['AmazonOrderId']
-								}, function(error, responseOrderItem){
+								    'AmazonOrderId' : order['AmazonOrderId'],
+								}); 
+								try{
+									let product = await amazonMws.products.searchFor({
+								        'Version': '2011-10-01',
+								        'Action': 'GetLowestPricedOffersForSKU',
+								        'SellerId': res['seller_id'],
+									    'MWSAuthToken': res['mws_auth_token'],
+						             	'MarketplaceId': res['market_place_id'],
+								        'SellerSKU': responseOrderItem.OrderItems.OrderItem.SellerSKU,
+								        'ItemCondition': 'New'
+								    });
 									var notification = new OneSignal.Notification({    
 									    contents: {    
-									        en: responseOrderItem.OrderItems.OrderItem.QuantityOrdered + ' ' + (responseOrderItem.OrderItems.OrderItem.Title.length > 20? responseOrderItem.OrderItems.OrderItem.Title.slice(0,20)+'...' : responseOrderItem.OrderItems.OrderItem.Title)
-													+  (order.OrderTotal? ' for ' + order.OrderTotal.Amount  + ' ' + order.OrderTotal.CurrencyCode : ''),
+									        en: responseOrderItem.OrderItems.OrderItem.QuantityOrdered + ' ' + (responseOrderItem.OrderItems.OrderItem.Title.length > 20? responseOrderItem.OrderItems.OrderItem.Title.slice(0,20)+'...' : responseOrderItem.OrderItems.OrderItem.Title) + 
+									        	' for ' + ( parseFloat(product.Summary.ListPrice.Amount) * parseInt(responseOrderItem.OrderItems.OrderItem.QuantityOrdered) ).toString() + ' ' + product.Summary.ListPrice.CurrencyCode
 									    },
 									    title: 'New Order'
 									});  
-									// <quantity> <product name > for <amount> <currency>. 
 									notification.postBody["filters"] = [{"field": "tag", "key": "userId", "relation": "=" ,"value": ObjectId(res['_id'])}];
 									notification.postBody["included_segments"] = ["Active Users"];    
 									notification.postBody["excluded_segments"] = ["Banned Users"];								
@@ -87,10 +94,17 @@ cron.schedule('5 * * * *', () => {
 								    .catch(function (err) {
 								        console.log('Something went wrong...', err);
 								    });
-								})
-				       		}
-						});
-					});
+							    }catch(error){
+				        			console.log('~~~~~~~~~~~ too many request ~~~~~~~~~~~ error ', error);
+							    }
+
+							} catch(error){
+				        		console.log('~~~~~~~~~~~ too many request ~~~~~~~~~~~ error ', error);
+							}
+						}
+		         	} catch(error) {
+				        console.log('~~~~~~~~~~~ too many request ~~~~~~~~~~~ error ', error);
+					}
 					delete res.last_date;
 				    collection
 			      	.update(
